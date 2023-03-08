a690f0d71892e7a05a5c0404f6ed5556b75758d1
毛子 <smallg20017@gmail.com>
2023-03-09 03:15:04 +0800

    Day8 | 使用 ORM 與資料庫進行互動

--- main.go
+++ main.go
@@ -1,33 +1,49 @@
-package main // 宣告 main package
+package main
 
 import (
-	"database/sql" // 引用 Go 標準庫 database/sql
-	"fmt"          // 引用 Go 標準庫 fmt
+	"fmt"
+	"log"
 
-	_ "github.com/go-sql-driver/mysql" // 引用 MySQL 驅動程式
+	"gorm.io/driver/mysql" // 引入 gorm 支援的 mysql driver
+	"gorm.io/gorm"         // 引入 gorm 套件
 )
 
 const (
-	USERNAME = "demo"      // 常數 USERNAME 為 demo
-	PASSWORD = "demo123"   // 常數 PASSWORD 為 demo123
-	NETWORK  = "tcp"       // 常數 NETWORK 為 tcp
-	SERVER   = "127.0.0.2" // 常數 SERVER 為 127.0.0.2
-	PORT     = 3306        // 常數 PORT 為 3306
-	DATABASE = "demo"      // 常數 DATABASE 為 demo
+	USERNAME = "demo"
+	PASSWORD = "demo123"
+	NETWORK  = "tcp"
+	SERVER   = "127.0.0.1"
+	PORT     = 3306
+	DATABASE = "demo"
 )
 
-func main() {
-	conn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s", USERNAME, PASSWORD, NETWORK, SERVER, PORT, DATABASE)
-	db, err := sql.Open("mysql", conn)
-	if err != nil {
-		fmt.Println("開啟 MySQL 連線發生錯誤，原因為：", err)
-		return
-	}
-	if err := db.Ping(); err != nil {
-		fmt.Println("資料庫連線錯誤，原因為：", err.Error())
-		return
-	}
-	defer db.Close()
+type User struct {
+	ID       int64  `json:"id" gorm:"primary_key;auto_increase'"`
+	Username string `json:"username"` // 使用者名稱
+	Password string `json:""`         // 密碼
+}
 
-	InsertUser(db, "test", "test")
+func main() {
+	dsn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", USERNAME, PASSWORD, NETWORK, SERVER, PORT, DATABASE)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) // 透過 dsn 連接資料庫
+	if err != nil {
+		panic("使用 gorm 連線 DB 發生錯誤，原因為 " + err.Error())
+	}
+	if err := db.AutoMigrate(new(User)); err != nil { // 自動建立 User 資料表
+		panic("資料庫 Migrate 失敗，原因為 " + err.Error())
+	}
+
+	user := &User{
+		Username: "test", // 使用者名稱
+		Password: "test", // 密碼
+	}
+	if err := CreateUser(db, user); err != nil { // 在資料庫中新增一筆 user 資料
+		panic("資料庫 Migrate 失敗，原因為 " + err.Error())
+	}
+
+	if user, err := FindUser(db, 1); err == nil { // 查詢符合條件的 user 資料
+		log.Println("查詢到 User 為 ", user)
+	} else {
+		panic("查詢 user 失敗，原因為 " + err.Error())
+	}
 }
--- sql.go
+++ sql.go
@@ -1,50 +1,16 @@
 package main // 宣告這是一個 main package
 
 import (
-	"database/sql" // 引用 Go 語言的資料庫 SQL 操作函式庫
-	"fmt"          // 引用 Go 語言的標準輸出函式庫
-
-	_ "github.com/go-sql-driver/mysql" // 引用 MySQL 驅動程式
-	// "_" 表示只要匯入該套件的 init() 函式，不使用該套件的其他函式
+	"gorm.io/gorm" // 引入 gorm 套件
 )
 
-type User struct { // 宣告一個 User struct 結構體
-	ID       string // 使用者 ID
-	Username string // 使用者名稱
-	Password string // 使用者密碼
+func CreateUser(db *gorm.DB, user *User) error {
+	return db.Create(user).Error // 在資料庫中新增一筆 user 資料
 }
 
-func CreateTable(db *sql.DB) error { // 宣告 CreateTable 函式，建立資料庫 Table
-	sql := `CREATE TABLE IF NOT EXISTS users(
-	id INT(4) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-        username VARCHAR(64),
-        password VARCHAR(64)
-	); ` // SQL 語句
-
-	if _, err := db.Exec(sql); err != nil { // 執行 SQL 語句
-		fmt.Println("建立 Table 發生錯誤:", err) // 輸出錯誤訊息
-		return err
-	}
-	fmt.Println("建立 Table 成功！") // 輸出成功訊息
-	return nil
-}
-
-func InsertUser(DB *sql.DB, username, password string) error { // 宣告 InsertUser 函式，插入一個使用者資料
-	_, err := DB.Exec("insert INTO users(username,password) values(?,?)", username, password) // 執行 SQL 語句，插入一個使用者資料
-	if err != nil {
-		fmt.Printf("建立使用者失敗，原因是：%v", err) // 輸出錯誤訊息
-		return err
-	}
-	fmt.Println("建立使用者成功！") // 輸出成功訊息
-	return nil
-}
-
-func QueryUser(db *sql.DB, username string) { // 宣告 QueryUser 函式，查詢使用者資料
-	user := new(User)                                                          // 創建一個 User struct 結構體
-	row := db.QueryRow("select * from users where username=?", username)       // 執行 SQL 語句，查詢使用者資料
-	if err := row.Scan(&user.ID, &user.Username, &user.Password); err != nil { // 讀取查詢結果，並映射到 User struct 結構體中
-		fmt.Printf("映射使用者失敗，原因為：%v\n", err) // 輸出錯誤訊息
-		return
-	}
-	fmt.Println("查詢使用者成功", *user) // 輸出成功訊息及使用者資料
+func FindUser(db *gorm.DB, id int64) (*User, error) {
+	user := new(User)            // 建立一個新的 User 變數
+	user.ID = id                 // 將 User 變數的 ID 欄位設定為參數中指定的 id
+	err := db.First(&user).Error // 在資料庫中查詢符合條件的第一筆 user 資料
+	return user, err             // 回傳查詢到的 user 資料及錯誤訊息
 }
