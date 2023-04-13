04af414c9568457877a99f3fb2e29b7e2b44e6a6
毛子 <smallg20017@gmail.com>
2023-04-14 04:18:12 +0800

    增加session

--- router.go
+++ router.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
+// SetupRouter 設定路由器並回傳 gin.Engine 物件
+// db 為 GORM 資料庫連線物件
 func SetupRouter(db *gorm.DB) *gin.Engine {
+	// 建立 gin 伺服器物件
 	server := gin.Default()
+	store := cookie.NewStore([]byte("secret"))
+	server.Use(sessions.Sessions("mysession", store))
+	// 載入 HTML 模板檔案
 	server.LoadHTMLGlob("static/html/*")
+
+	// 設定靜態檔案目錄
 	server.Static("/static", "./static")
+
+	// 設定路由器
 	server.GET("/login", LoginPage)
 	server.POST("/login", LoginAuth(db))
 	server.GET("/register", RegisterPage)
 	server.POST("/register", RegisterAuth(db))
 	server.GET("/index", IndexPage)
+	// server.GET("/admin", AdminPage)
+
+	// 回傳路由器
 	return server
+
 }
--- service.go
+++ service.go
@@ -1,53 +1,13 @@
 package main // 包名為 main，表明這是一個可執行的 Go 程序
 
 import (
-	"errors" // 引入 errors 包，用於返回錯誤
-	"go_web_learning/model"
+	"errors"   // 引入 errors 包，用於返回錯誤
 	"net/http" // 引入 net/http 包，用於處理 HTTP 請求
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin" // 引入 gin 框架
 	"gorm.io/gorm"             // 引入 gorm 框架
 )
-
-// Login 函數，用於驗證用戶名和密碼是否正確
-func Login(db *gorm.DB, username string, password string) error {
-	user, err := FindUser(db, username)
-	if err != nil {
-		return errors.New("使用者不存在")
-	}
-	if user.Password != password {
-		return errors.New("密碼錯誤")
-	}
-	return nil
-}
-
-// LoginPage 函數，用於顯示登入頁面
-func LoginPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "login.html", nil)
-}
-
-// Register 函數，用於註冊新用戶，驗證用戶名和密碼是否正確
-func Register(db *gorm.DB, username string, password string) error {
-	_, err := FindUser(db, username)
-	if err == nil {
-		return errors.New("使用者已存在")
-	} else {
-		return CreateUser(db, &model.User{
-			Username: username,
-			Password: password,
-		})
-	}
-}
-
-// RegisterPage 函數，用於顯示註冊頁面
-func RegisterPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "register.html", nil)
-}
-
-// IndexPage 函數，用於顯示登入頁面
-func IndexPage(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", nil)
-}
 
 // ValidateForm 函數，用於驗證表單參數是否正確
 func ValidateForm(c *gin.Context, usernameField string, passwordField string) (string, string, error) {
@@ -108,6 +68,11 @@ func LoginAuth(db *gorm.DB) gin.HandlerFunc {
 			return
 		}
 		if err := Login(db, username, password); err == nil {
+			// 設定 session 變數
+			session := sessions.Default(c)
+			session.Set("username", username)
+			session.Save()
+
 			c.HTML(http.StatusOK, "login.html", gin.H{
 				"success":  "登入成功 2秒後將導向至內容頁面",
 				"redirect": "/index",
